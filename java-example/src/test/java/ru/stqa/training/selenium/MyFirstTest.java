4a1e165d604ac85029e5881d27e0493ffde39516
squirrelnut <squirrel_nut@inbox.ru>
2017-11-29 14:47:44 +0300

    Создан новый проект

---
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -0,0 +1,33 @@
+package ru.stqa.training.selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() throws InterruptedException {
+        driver.get("http://www.mail.ru/");
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}

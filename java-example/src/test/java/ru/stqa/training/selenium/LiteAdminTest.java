2b4b6cce84aa13d4639374f2fbef7c7df822b999
squirrelnut <squirrel_nut@inbox.ru>
2017-11-29 20:12:21 +0300

    Задание №3

--- java-example/src/test/java/ru/stqa/training/selenium/LiteAdminTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LiteAdminTest.java
@@ -8,22 +8,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.concurrent.TimeUnit;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
-public class MyFirstTest {
+public class LiteAdminTest {
     private WebDriver driver;
     private WebDriverWait wait;
 
     @Before
     public void start() {
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }
 
     @Test
     public void myFirstTest() throws InterruptedException {
-        driver.get("http://www.yandex.ru");
-
+        driver.get("http://localhost/litecart/admin/");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @After

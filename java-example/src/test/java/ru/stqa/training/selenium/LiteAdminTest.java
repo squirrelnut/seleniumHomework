451e3c86c082a3a71d6b8dfb47481e7edb739ab8
squirrelnut <squirrel_nut@inbox.ru>
2017-12-01 22:22:26 +0300

    Запуск FF Nightly по новой схеме

--- java-example/src/test/java/ru/stqa/training/selenium/LiteAdminTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LiteAdminTest.java
@@ -4,8 +4,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
@@ -18,7 +21,28 @@ public class LiteAdminTest {
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        // вызов драйверов браузеров по новой схеме
+        //driver = new InternetExplorerDriver();
+      //  driver = new FirefoxDriver();
+       // driver = new ChromeDriver();
+
+        //Вызов Firefix по старой схеме:
+//        FirefoxOptions options = new FirefoxOptions()
+//                .setBinary("C:\\Program Files\\Mozilla Firefox ESR45\\firefox.exe")
+//                .setLegacy(true);
+//        driver = new FirefoxDriver(options);
+
+
+
+       // Вызов Firefox Nightly по новой схеме:
+        driver = new FirefoxDriver();
+        FirefoxOptions options = new FirefoxOptions()
+                .setBinary("C:\\Program Files\\Nightly\\firefox.exe")
+                .setLegacy(false);
+        driver = new FirefoxDriver(options);
+
+
+        System.out.println(((HasCapabilities) driver).getCapabilities()); // Печать в консоль настроек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         wait = new WebDriverWait(driver, 10);
     }

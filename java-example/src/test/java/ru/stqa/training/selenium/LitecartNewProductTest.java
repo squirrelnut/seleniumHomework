5e7b112b24a22c09f27aadf372f81fb7e8080c61
squirrelnut <squirrel_nut@inbox.ru>
2017-12-16 03:19:30 +0300

    ДЗ № 12
    Сценарий добавления нового товара в админке

--- java-example/src/test/java/ru/stqa/training/selenium/LitecartNewProductTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/LitecartNewProductTest.java
@@ -1,22 +1,16 @@
 package ru.stqa.training.selenium;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.HasCapabilities;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 public class LitecartNewProductTest extends TestBase {
     @Test
-    public void testLitecartNewProductTest() {
+    public void testLitecartNewProductTest() throws IOException {
         // Входим в админку
         driver.get("http://localhost/litecart/admin/");
         driver.findElement(By.name("username")).sendKeys("admin");
@@ -29,35 +23,91 @@ public class LitecartNewProductTest extends TestBase {
 
         // заполняем вкладку General
         driver.findElement(By.cssSelector("label input[value='1']")).click(); // выбираем Enabled
-        driver.findElement(By.cssSelector("input[name='name[en]']")).sendKeys("Life Ring"); // название товара
-        driver.findElement(By.cssSelector("input[name='code']")).sendKeys("lr001");
-        putCheckbox("input[name='categories[]'][data-name='Root']"); // выбираем чек-бокс Root
-        fillSelect("select[name='default_category_id']","Root"); // в выпад. списке выбираем Root
-        putCheckbox("input[name='product_groups[]'][value='1-3']");// выбираем чек-бокс Unisex
-        driver.findElement(By.cssSelector("input[name='quantity']")).sendKeys("10");
+        fillInput("input[name='name[en]']","Life Ring"); //  название товара
+        fillInput("input[name='code']", "lr001"); //  код товара
+        putCheckbox("input[name='categories[]'][data-name='Root']"); //  чек-бокс Root
+        fillSelect("select[name='default_category_id']", "Root"); // выбираем Root
+        putCheckbox("input[name='product_groups[]'][value='1-3']");//  чек-бокс Unisex
+        fillInput("input[name='quantity']", "10");
         fillSelect("select[name='quantity_unit_id']", "pcs");
         fillSelect("select[name='delivery_status_id']", "3-5 days");
         fillSelect("select[name='sold_out_status_id']", "Sold out");
+        attachFile("input[name='new_images[]']", "img\\lifering.png");  // Прикрепляем файл
+        // заполняем даты
+        driver.findElement(By.cssSelector("input[name='date_valid_from']")).sendKeys("2017-12-12");
+        driver.findElement(By.cssSelector("input[name='date_valid_to']")).sendKeys("2018-12-12");
 
-        driver.findElement(By.cssSelector("input[name='new_images[]']")).sendKeys("lifering.png");
-        driver.findElement(By.cssSelector("input[name='date_valid_from']")).sendKeys("12122017");
-        driver.findElement(By.cssSelector("input[name='date_valid_to']")).sendKeys("12122018");
+        //Переходим на вкладку Information и заполняем ее
+        driver.findElement(By.linkText("Information")).click();
+        pause(2000); // пауза
+        fillSelect("select[name='manufacturer_id']", "ACME Corp.");
+        fillInput("input[name='short_description[en]']", "Life ring is means to assist someone who is drowning.");
+        fillTextarea("textarea[name='description[en]']");
+
+
+        //Переходим на вкладку Information и заполняем ее
+        driver.findElement(By.linkText("Prices")).click();
+        pause(2000); // пауза
+        fillInput("input[name='purchase_price']", "100");
+        fillSelect("select[name='purchase_price_currency_code']", "US Dollars");
+        fillInput("input[name='prices[USD]']", "100");
+
+        // Сохраняем новый товар
+        driver.findElement(By.cssSelector("button[name='save']")).click();
+
+        // Проверяем, что новый товар появился
+        driver.findElement(By.linkText("Life Ring")).click();
 
     }
 
-    public void putCheckbox(String locator){
-         WebElement checkbox = driver.findElement(By.cssSelector(locator));
-         if (checkbox.isSelected()){
+    public void putCheckbox(String locator) {
+        WebElement checkbox = driver.findElement(By.cssSelector(locator));
+        if (checkbox.isSelected()) {
             return;
         } else {
             checkbox.click();
         }
     }
 
-    public void fillSelect(String locator, String selectedItem){
+    public void fillInput(String locator, String text) {
+        WebElement element = driver.findElement(By.cssSelector(locator));
+        element.clear();
+        element.sendKeys(text);
+
+    }
+
+    public void fillSelect(String locator, String selectedItem) {
         WebElement selectElement = driver.findElement(By.cssSelector(locator));
         Select select = new Select(selectElement);
         //List options = select.getOptions();
         select.selectByVisibleText(selectedItem);
+    }
+
+    public void attachFile(String locaror, String fileName) {
+        File file = new File(fileName);
+        if (file.exists()) {
+            driver.findElement(By.cssSelector(locaror)).sendKeys(file.getAbsolutePath());
+            System.out.println("Файл " + file.getAbsolutePath() + " прикреплен");
+        } else {
+            System.out.println("Файл " + file.getAbsolutePath() + " не существует");
+        }
+
+
+    }
+
+    public void pause(Integer milliseconds) {
+        try {
+            TimeUnit.MILLISECONDS.sleep(milliseconds);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void fillTextarea(String locator) {
+        WebElement element = driver.findElement(By.cssSelector(locator));
+        element.clear();
+        String text = "Life ring is designed to be thrown to a person in the water, to provide buoyancy and prevent drowning.";
+        element.sendKeys(text);
+
     }
 }
--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -23,8 +23,8 @@ public class TestBase {
 //                .setLegacy(true);
 //        driver = new FirefoxDriver(options);
 
-//        driver = new ChromeDriver();
-//        driver = new InternetExplorerDriver();
+       //driver = new ChromeDriver();
+       // driver = new InternetExplorerDriver();
         driver = new FirefoxDriver();
         System.out.println(((HasCapabilities) driver).getCapabilities()); // Печать в консоль настроек.
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
